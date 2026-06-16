a3bcd8fb2896c5189c68c98a6152f1c9c0f6d549
jgs-jeeves <jeeves@johngalt.id>
2026-06-16 03:32:45 +0000

    chore: remove tmp-commit.cjs

--- tmp-commit.cjs
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-const o = { cwd: 'D:\\repos\\karmaniverous\\jeeves-scripts-template', encoding: 'utf8' };
-execSync('git add -A', o);
-console.log(execSync('git diff --cached --stat', o));
-console.log(execSync('git commit -m "fix: move PIPELINE_CONFIG_PATH to scripts dir (was incorrectly in jeeves-core config)"', o));
-console.log(execSync('git push', o));
